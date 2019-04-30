e87016ad1d00ff7e536ef6c0507ba81574567061
vvignatov <inaburat@gmail.com>
2019-04-30 19:00:43 +0300

    correction

--- templates/index.php
+++ templates/index.php
@@ -4,7 +4,7 @@
 <ul class="promo__list">
     <!--заполните этот список из массива категорий-->
     <?php foreach ($categories as $key => $value):?>
-        <li class="promo__item promo__item--<?=strip_tags($value['mod'])?>">
+        <li class="promo__item promo__item--<?=$value['mod']?>">
             <a class="promo__link" href="pages/all-lots.html"><?=$value['name'];?></a>
         </li>
     <?php endforeach;?>
@@ -19,18 +19,21 @@
     <?php foreach ($lots as $key => $value):?>
         <li class="lots__item lot">
             <div class="lot__image">
-                <img src="<?=strip_tags($value['image']);?>" width="350" height="260" alt="">
+                <img src="<?=$value['image'];?>" width="350" height="260" alt="">
             </div>
             <div class="lot__info">
                 <span class="lot__category"><?=$value['category'];?></span>
-                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=strip_tags($value['name']);?></a></h3>
+                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value['name']);?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
                         <span class="lot__amount">Стартовая цена</span>
-                        <span class="lot__cost"><?=getPrice(strip_tags($value['price']));?></span>
+                        <span class="lot__cost"><?=getPrice($value['price']);?></span>
                     </div>
                     <div class="lot__timer timer">
-                        12:23
+                        <?php
+                            $ny_date = strtotime("now");
+                            print($ny_date);
+                        ?>
                     </div>
                 </div>
             </div>
